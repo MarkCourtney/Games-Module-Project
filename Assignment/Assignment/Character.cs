96ab34c35ee90c9a476d67bb35152c955ff711f6
Ciaran Schutte <ciaranschutte@gmail.com>
2012-03-31 22:54:23 +0100

    Intial commit.

---
+++ Assignment/Assignment/Character.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Assignment
+{
+    class Character : DrawableGameComponent
+    {
+        private Texture2D picture;
+        private float rotation;
+        private float speed;
+        private Vector2 rotationVector;
+        private Vector2 location;
+        private Vector2 localLocation;
+        private SpriteBatch sb;
+        private bool hasRotated;
+        private PanningBackground background;
+
+        public Character(Game g, Texture2D picture, float startX, float startY, float speed, SpriteBatch sb) : base(g)
+        {
+            this.picture = picture;
+            this.speed = speed;
+            this.sb = sb;
+            rotation = 0.0f;
+            rotationVector = new Vector2(0.0f, 0.01f);
+            location = new Vector2(startX, startY);
+            hasRotated = false;
+        }
+
+        public Vector2 GetLocation()
+        {
+            return location;
+        }
+
+        public int GetHeight()
+        {
+            return picture.Height;
+        }
+
+        public int GetWidth()
+        {
+            return picture.Width;
+        }
+
+        public void SetBackground(PanningBackground background)
+        {
+            this.background = background;
+        }
+
+        public float GetRotation()
+        {
+            return rotation;
+        }
+        
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            KeyboardState k = Keyboard.GetState();
+
+            // If turning left
+            if (k.IsKeyDown(Keys.Left))
+            {
+                rotation -= 0.01f;
+                rotationVector.X = (float) Math.Sin(rotation);
+                rotationVector.Y = (float) -Math.Cos(rotation);
+                hasRotated = true;
+            }
+
+            // If turning right 
+            else if (k.IsKeyDown(Keys.Right))
+            {
+                rotation += 0.01f;
+                rotationVector.X = (float) Math.Sin(rotation);
+                rotationVector.Y = (float) -Math.Cos(rotation);
+                hasRotated = true;
+            }
+
+            // Forwards
+            else if (k.IsKeyDown(Keys.Up))
+            {
+                location.X += speed * rotationVector.X;
+                if (hasRotated)
+                {
+                    location.Y += speed * rotationVector.Y;
+                }
+                else
+                {
+                    location.Y -= speed;
+                }
+            }
+
+            // Backwards
+            else if (k.IsKeyDown(Keys.Down))
+            {
+                location.X -= speed * rotationVector.X;
+                if (hasRotated)
+                {
+                    location.Y -= speed * rotationVector.Y;
+                }
+                else
+                {
+                    location.Y += speed;
+                }
+            }
+
+            if (background != null) localLocation = background.ResolveLocation(location);
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            if (background == null) return;
+            sb.Begin();
+            sb.Draw(picture, new Rectangle((int)localLocation.X, (int)localLocation.Y, picture.Width, picture.Height), null, Color.White, rotation, new Vector2(picture.Width / 2, picture.Height / 2), SpriteEffects.None, 0);
+            sb.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Assignment/Assignment/PanningBackground.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Assignment
+{
+    class PanningBackground : DrawableGameComponent
+    {
+        private Character character;
+        private Rectangle section;
+        private Texture2D image;
+        private SpriteBatch sb;
+        //private Rectangle core, whole;
+        private Rectangle top, left, right, bottom;
+        private int charH, charW;
+        private float speed;
+
+        public PanningBackground(Game g, Character character, Texture2D image, SpriteBatch sb) : base(g)
+        {
+            this.character = character;
+            this.image = image;
+            this.sb = sb;
+            charH = character.GetHeight();
+            charW = character.GetWidth();
+            speed = 5;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            section = new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Width);
+            /*core = new Rectangle((GraphicsDevice.Viewport.Width / 2) + 1, (GraphicsDevice.Viewport.Height / 2) + 1, image.Width - (GraphicsDevice.Viewport.Width), image.Height - (GraphicsDevice.Viewport.Height));
+            whole = new Rectangle(0, 0, image.Width, image.Height);*/
+            top = new Rectangle(0, 0, image.Width, GraphicsDevice.Viewport.Height / 2);
+            bottom = new Rectangle(0, image.Height - (GraphicsDevice.Viewport.Height / 2), image.Width, GraphicsDevice.Viewport.Height / 2);
+            left = new Rectangle(0, 0, GraphicsDevice.Viewport.Width / 2, image.Height);
+            right = new Rectangle(image.Width - (GraphicsDevice.Viewport.Width / 2), 0, GraphicsDevice.Viewport.Width / 2, image.Height);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Vector2 l = character.GetLocation();
+            int xCentre = (int)l.X;
+            int yCentre = (int)l.Y;
+
+            Rectangle lRect = new Rectangle(xCentre, yCentre, charW, charH);
+            /*if (core.Intersects(lRect))
+            {
+                section = new Rectangle(xCentre - (GraphicsDevice.Viewport.Width / 2), yCentre - (GraphicsDevice.Viewport.Height / 2), GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+            }*/
+            if (lRect.Intersects(top) || lRect.Intersects(bottom))
+            {
+                yCentre = section.Y + (GraphicsDevice.Viewport.Height / 2);
+            }
+            if (lRect.Intersects(left) || lRect.Intersects(right))
+            {
+                xCentre = section.X + (GraphicsDevice.Viewport.Width / 2);
+            }
+            section = new Rectangle(xCentre - (GraphicsDevice.Viewport.Width / 2), yCentre - (GraphicsDevice.Viewport.Height / 2), GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+            
+    
+            base.Update(gameTime);
+        }
+
+        public Vector2 ResolveLocation(Vector2 globalLocation)
+        {
+            return new Vector2(globalLocation.X - section.X, globalLocation.Y - section.Y);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            sb.Begin();
+            sb.Draw(image, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), section, Color.White);
+            sb.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
